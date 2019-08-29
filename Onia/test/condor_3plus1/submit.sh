ec96ebb09f29b6bfa35f3a14da77087f6f4a21d4
Candan Dozen <candan.dozen@cern.ch>
2019-08-29 11:04:41 -0500

    modified condor script for mixing

--- Onia/test/condor_3plus1/submit.sh
+++ Onia/test/condor_3plus1/submit.sh
@@ -1,10 +1,10 @@
 cd condor2017B
 ./SplitAndRun.sh
-cd condor2017C
-./SplitAndRun.sh
-cd ../condor2017D
-./SplitAndRun.sh
-cd ../condor2017E
-./SplitAndRun.sh
-cd ../condor2017F
-./SplitAndRun.sh
+#cd condor2017C
+#./SplitAndRun.sh
+#cd ../condor2017D
+#./SplitAndRun.sh
+#cd ../condor2017E
+#./SplitAndRun.sh
+#cd ../condor2017F
+#./SplitAndRun.sh
--- Onia/test/runMixingRootupler_twoInputs.py
+++ Onia/test/runMixingRootupler_twoInputs.py
@@ -21,7 +21,9 @@ process.source = cms.Source("PoolSource",
 #'file:/eos/uscms/store/user/lpcmuon/fourmuonMC/H0ToUps1SMuMu_m18p5_TuneCUEP8M1_13TeV-pythia8/BPHSkim-v10/180308_232212/0000/BPHSkim_1.root',
 #'file:/eos/uscms/store/user/l1upgrades/Run2017/fourmuon/MuOnia/BPHSkim-v4-Run2017F-17Nov2017-v1/180314_061742/0000/BPHSkim_101.root')
 #'file:/eos/uscms/store/user/lpcbphy/hui/MuOnia/BPHSkim-2018B_Rereco-Run2018B-17Sep2018-v1/190104_025327/0000/BPHSkim_426.root'),
-'file:/eos/uscms/store/user/muahmad/FourMuon_Analysis/MuOnia/2017_v2/MuOnia/BPHSkim--Run2017B-17Nov2017-v1/190609_091355/0000/BPHSkim_2017_10.root'),
+'root://cmsxrootd.fnal.gov//store/user/muahmad/FourMuon_Analysis/MuOnia_v3/2017_v2/MuOnia/BPHSkim--Run2017F-17Nov2017-v1/190611_021142/0000/BPHSkim_2017_99.root')
+#'file:/eos/uscms/store/user/muahmad/FourMuon_Analysis/MuOnia_v3/2017_v2/MuOnia/BPHSkim--Run2017F-17Nov2017-v1/190611_021142/0000/BPHSkim_2017_99.root')
+#'file:/eos/uscms/store/user/muahmad/FourMuon_Analysis/MuOnia/2017_v2/MuOnia/BPHSkim--Run2017B-17Nov2017-v1/190609_091355/0000/BPHSkim_2017_10.root'),
 #eventsToProcess = cms.untracked.VEventRange('317641:1352331850-317641:1352331850')
 )
 
@@ -39,13 +41,14 @@ process.rootuple.upsilon_mass = cms.double(9.4603)
 process.rootuple.triggerCuts = cms.uint32(36)
 process.rootuple.isMC = cms.bool(False)                 # is mc?
 process.rootuple.onia_mass_cuts = cms.vdouble(0.1,500)    # you may need to adjust this
-
 process.rootuple.SecondSource.fileNames = cms.untracked.vstring(
-'file:/eos/uscms/store/user/muahmad/FourMuon_Analysis/MiniBias/2017_v2/ZeroBias/BPHSkim--Run2017B-17Nov2017-v1/190609_091136/0000/BPHSkim_2017_MiniBias_97.root'
+#'file:/eos/uscms/store/user/muahmad/FourMuon_Analysis/MiniBias/2017_v2/ZeroBias/BPHSkim--Run2017B-17Nov2017-v1/190609_091136/0000/BPHSkim_2017_MiniBias_99.root'
+'root://cmsxrootd.fnal.gov//store/user/muahmad/FourMuon_Analysis/MiniBias/2017_v2/ZeroBias/BPHSkim--Run2017B-17Nov2017-v1/190609_091136/0000/BPHSkim_2017_MiniBias_97.root')
+#'file:/eos/uscms/store/user/muahmad/FourMuon_Analysis/MiniBias/2017_v2/ZeroBias/BPHSkim--Run2017B-17Nov2017-v1/190609_091136/0000/BPHSkim_2017_MiniBias_97.root'
 #'file:/eos/uscms/store/user/l1upgrades/Run2017/fourmuon/ZeroBias/BPHSkim-v6-Run2017F-17Nov2017-v1/180321_073042/0000/BPHSkim_250.root',
 #'file:/eos/uscms/store/user/l1upgrades/Run2017/fourmuon/ZeroBias/BPHSkim-v6-Run2017F-17Nov2017-v1/180321_073042/0000/BPHSkim_250.root'
 #    'file:/eos/cms/store/user/jblee/SpyFEDemulated234824.root'
-	  )
+	  #)
 
 
 
